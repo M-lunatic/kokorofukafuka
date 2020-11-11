5c8b7c064296aee596127a889dd91470284ed1ed
Mizuki <mirrorsound0623@gmail.com>
2020-11-11 18:02:15 +0900

    モデルの追記

--- app/Diagnosis.php
+++ app/Diagnosis.php
@@ -7,4 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 class Diagnosis extends Model
 {
     //
+    protected $fillable = [            
+            'EorI1' ,
+            'EorI2' ,
+            'EorI3' ,
+            'EorI4' ,
+            'EorI5' ,
+            'SorN1' ,
+            'SorN2' ,
+            'SorN3' ,
+            'SorN4' ,
+            'SorN5' ,
+            'TorF1' ,
+            'TorF2' ,
+            'TorF3' ,
+            'TorF4' ,
+            'TorF5' ,
+            'JorP1' ,
+            'JorP2' ,
+            'JorP3' ,
+            'JorP4' ,
+            'JorP5' ,
+        ];
+        protected $hidden = [
+            //
+            'user_id',
+        ];
 }
