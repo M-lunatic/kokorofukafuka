aca95d7bcab31599925c7d79741eb5f6d9129b2f
Mizuki <mirrorsound0623@gmail.com>
2020-11-11 18:42:00 +0900

    診断ページのvalue値と変数の書き換え

--- resources/views/diagnosises/diagnosis-16type-result.blade.php
+++ resources/views/diagnosises/diagnosis-16type-result.blade.php
@@ -6,27 +6,28 @@
 <div>
 <h1>診断結果画面</h1>
 
-<h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $type }}です</h2>
+<h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $data->$type }}です</h2>
 
 
 
-
+@foreach()
 
 <tr>
 
-    <td><p>外向:</p>{{ $XXX->$Epoint }}</td>
-    <td><p>内向:</p>{{ $XXX->$Ipoint }}</td>
-    <td><p>感覚:</p>{{ $XXX->$Spoint }}</td>
-    <td><p>直観:</p>{{ $XXX->$Npoint }}</td>
-    <td><p>思考:</p>{{ $XXX->$Tpoint }}</td>
-    <td><p>感情:</p>{{ $XXX->$Fpoint }}</td>
-    <td><p>計画:</p>{{ $XXX->$Jpoint }}</td>
-    <td><p>知覚:</p>{{ $XXX->$Ppoint }}</td>
+    <td><p>外向:</p>{{ $data->$Epoint }}</td>
+    <td><p>内向:</p>{{ $data->$Ipoint }}</td>
+    <td><p>感覚:</p>{{ $data->$Spoint }}</td>
+    <td><p>直観:</p>{{ $data->$Npoint }}</td>
+    <td><p>思考:</p>{{ $data->$Tpoint }}</td>
+    <td><p>感情:</p>{{ $data->$Fpoint }}</td>
+    <td><p>計画:</p>{{ $data->$Jpoint }}</td>
+    <td><p>知覚:</p>{{ $data->$Ppoint }}</td>
 
 
 </tr>
 <!--検索させる-->
 
+@endforeach
 
 
 </div>
