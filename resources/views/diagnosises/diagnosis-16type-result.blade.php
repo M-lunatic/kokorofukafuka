9e16ea96a36809f6a8998df2cacc534ef8e2c196
Mizuki <mirrorsound0623@gmail.com>
2020-11-12 14:47:47 +0900

    タイプ表示だけにした＆Google検索できるようにした

--- resources/views/diagnosises/diagnosis-16type-result.blade.php
+++ resources/views/diagnosises/diagnosis-16type-result.blade.php
@@ -6,29 +6,25 @@
 <div>
 <h1>診断結果画面</h1>
 
-<h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $data->$type }}です</h2>
+<h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $type }}です</h2>
 
-
-
-@foreach()
-
-<tr>
-
-    <td><p>外向:</p>{{ $data->$Epoint }}</td>
-    <td><p>内向:</p>{{ $data->$Ipoint }}</td>
-    <td><p>感覚:</p>{{ $data->$Spoint }}</td>
-    <td><p>直観:</p>{{ $data->$Npoint }}</td>
-    <td><p>思考:</p>{{ $data->$Tpoint }}</td>
-    <td><p>感情:</p>{{ $data->$Fpoint }}</td>
-    <td><p>計画:</p>{{ $data->$Jpoint }}</td>
-    <td><p>知覚:</p>{{ $data->$Ppoint }}</td>
-
-
-</tr>
 <!--検索させる-->
-
-@endforeach
-
+<div class="gsearch">
+<p>Let's Search!</p><br />
+  <script>
+  (function() {
+    var cx = '++++++++++++++++++++:++++++++++';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+  <script>
+<gcse:searchbox-only></gcse:searchbox-only>
+ </div>
 
 </div>
 
