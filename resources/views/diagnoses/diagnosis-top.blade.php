e7031b44516d1f4a9af5df855cfab75eced4d265
Mizuki <mirrorsound0623@gmail.com>
2020-11-13 18:56:39 +0900

    診断結果一覧を見れるようにしたい、一時保存

--- resources/views/diagnoses/diagnosis-top.blade.php
+++ resources/views/diagnoses/diagnosis-top.blade.php
@@ -3,14 +3,22 @@
 @section('content')
 
 <div>
-<h1>診断一覧</h1>
-<a href ="{{ route('diagnosis.create') }}">16タイプ性格診断</a>
-
-
+    <h1>診断一覧</h1>
+    <a href ="{{ route('diagnosis.create') }}">16タイプ性格診断</a>
 
 </div>
 
+<div>
 
+<h1>過去の診断結果一覧</h1>
+    <!--結果があるときだけ表示する-->
+    <tr>
+        <td>タイプ<a href ="{{ route('diagnosis.show') }}">{{ }}</a></td>
+        <td>やった日{{ }}</td>
+        <td>やりなおした日{{ }}</td>
+    </tr>
+
+</div>
 
 
 @endsection
