10a8902e311f30e28812d56fc13c812d2b9baf20
Mizuki <mirrorsound0623@gmail.com>
2020-11-13 18:58:00 +0900

    サイト名の変更とマイページへのリンクを貼った

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    性格診断サイト
+                    Laravelで作った会員登録機能付き性格診断サイト
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -58,13 +58,25 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+
+                                    <a class ="dropdown-item" href="{{ route('mypage') }}" >{{__('mypage')}}</a>
+                                    
+                                    
+
+
                                 </div>
+                                
+                                    
+
+
+
+
                             </li>
                         @endguest
                     </ul>
