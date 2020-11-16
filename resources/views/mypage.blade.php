c1c214ba403c94e7fe17b3e2f95247c19a72157d
Mizuki <mirrorsound0623@gmail.com>
2020-11-16 18:58:42 +0900

    URI変更

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -6,7 +6,7 @@
 <div>
     <h1>{{Auth::user()->user_name}}さんのマイページ</h1>
     <ul>
-        <li><a href="{{ route('diagnosis.index') }}">診断結果一覧</a></li>
+        <li><a href="{{ route('diagnoses.index') }}">診断結果一覧</a></li>
         <li><a href="">登録情報の変更</a></li>
     </ul>
 
