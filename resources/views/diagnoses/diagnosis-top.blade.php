f4e5995cef54ad57f122e76676862db8d7dded28
Mizuki <mirrorsound0623@gmail.com>
2020-11-17 19:10:56 +0900

    削除機能追加

--- resources/views/diagnoses/diagnosis-top.blade.php
+++ resources/views/diagnoses/diagnosis-top.blade.php
@@ -3,20 +3,42 @@
 @section('content')
 
 <div>
-    <h1>診断一覧</h1>
-    <a href ="{{ route('diagnosis.create') }}">16タイプ性格診断</a>
+    <h1>診断TOP</h1>
+    <a href ="{{ route('diagnoses.create') }}">16タイプ性格診断</a>
 
 </div>
 
 <div>
 
-<h1>過去の診断結果一覧</h1>
+
+
+<h2>過去の診断結果一覧</h2>
+
+<p>{{ $msg }}</p><br>
+
+    @foreach($diagnosis as $dia)
+
     <!--結果があるときだけ表示する-->
-    <tr>
-        <td>タイプ<a href ="{{ route('diagnosis.show') }}">{{ }}</a></td>
-        <td>やった日{{ }}</td>
-        <td>やりなおした日{{ }}</td>
-    </tr>
+    <table border="1">
+        
+            <td>タイプ : <a href ="{{ route('diagnoses.show',$dia->id ) }}">{{ $dia->type }} </a></td>
+            <td>やった日時 : {{ $dia->created_at }} </td>
+            <td>やりなおした日時 : {{ $dia->updated_at }} </td>
+            <td>
+                <form action="{{ route('diagnoses.destroy',$dia->id )}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="削除" class="btn btn-danger">
+                </form>
+            </td>
+
+
+        
+    <table>    
+    
+    
+    @endforeach
+
 
 </div>
 
