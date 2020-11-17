32d59c78fe2d47432b5d9dea6ad9f95ea556f555
Mizuki <mirrorsound0623@gmail.com>
2020-11-17 19:11:40 +0900

    診断結果と結果一覧の詳細の役割の分割

--- resources/views/diagnoses/diagnosis-16type-show.blade.php
+++ resources/views/diagnoses/diagnosis-16type-show.blade.php
@@ -4,11 +4,11 @@
 @section('content')
 
 <div>
-    <h1>診断結果画面　詳細show</h1>
+    <h1>診断結果画面</h1>
 
-    <h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $diagnosis[1] }}です</h2>
+    <h2>{{Auth::user()->user_name}}さんの性格タイプは{{ $diagnosis->type }}です</h2>
 
-    <a href ="{{ url( $diagnosis[0] ) }}" >Let's search {{ $diagnosis[1] }}!</a>
+    <a href ="{{ url( $diagnosis->type_url ) }}" >Let's search {{ $diagnosis->type }}!</a>
     <p>※クリックするとGoogleの検索ページに飛びます。</p><br />
 
     <a href ="{{ route('diagnoses.index') }}">今までやった診断結果一覧へ</a>
